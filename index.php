908a3ab1ce145db9664569522555eb429fe5bf05
wahyuprasettya <wahyu.prasettya@gmail.com>
2019-04-25 19:38:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<center>
+	Haloooooo!!! Aplikasi Virus Sederhana!!!!! <br><br>
+
+	<form action="" method="post">
+	<button type="submit" name="klik">Klik Aku Dong!!!</button>
+	</form>
+</center>
+
+
+<?php
+
+if (isset($_POST['klik'])) {
+	
+while ( 0 <= 0) {
+
+	echo "bye";
+	echo'<br>';
+	}
+}
+
+?>
+
+</body>
+</html>
+
+
+
